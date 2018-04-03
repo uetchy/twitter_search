bdb9df6c418efc303edec6958fafe30f963a38e8
Yasuaki Uechi <y@uechi.io>
2018-04-03 19:42:08 +0900

    update deps

--- twitter_search/search.py
+++ twitter_search/search.py
@@ -1,5 +1,4 @@
-from requests_oauthlib import OAuth1Session, OAuth1
-import json
+from requests_oauthlib import OAuth1
 import requests
 import urllib
 import sys
@@ -7,7 +6,6 @@ import io
 import os
 import re
 from bs4 import BeautifulSoup
-from IPython import embed
 
 consumer_key = os.environ['TWITTER_CONSUMER_KEY']
 consumer_key_secret = os.environ['TWITTER_CONSUMER_KEY_SECRET']
@@ -25,19 +23,23 @@ if __name__ == '__main__':
     arxiv_title = soup.find('title').text
 
     # query search for twitter
-    query_word = urllib.parse.quote_plus(arxiv_url + " AND -filter:retweets AND -filter:replies")
+    query_word = urllib.parse.quote_plus(
+        arxiv_url + " AND -filter:retweets AND -filter:replies")
 
     url = "https://api.twitter.com/1.1/search/tweets.json?q=" + query_word
-    auth = OAuth1(consumer_key, consumer_key_secret, access_token, access_token_secret)
-    response = requests.get(url, auth = auth)
+    auth = OAuth1(consumer_key, consumer_key_secret, access_token,
+                  access_token_secret)
+    response = requests.get(url, auth=auth)
     print('rate-limit-remaining', response.headers['x-rate-limit-remaining'])
     data = response.json()['statuses']
 
-    #データ表示
+    # データ表示
     for tweet in data:
         text = re.sub('http.+\s?', '', tweet["text"])
         splitted_text = text.split()
-        splitted_text_without_title = [x for x in splitted_text if not x in arxiv_title.split()]
+        splitted_text_without_title = [
+            x for x in splitted_text if x not in arxiv_title.split()
+        ]
         score = len(splitted_text_without_title) / len(splitted_text)
         if score > 0.8 and len(text) > 10:
             print(text, score, len(text))
